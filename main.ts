256555ebf2645ce29fad901c786a28a64a00df8d
Jisu-1129 <115052208+Jisu-1129@users.noreply.github.com>
2022-10-12 09:36:24 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let counter = 0
+input.onButtonPressed(Button.A, function () {
+    counter = 0
+    while (counter <= 4) {
+        led.plot(counter, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        counter += 1
+    }
+})
