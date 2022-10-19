cec7f9f4149398683cb16cb0c3e21626eb58346c
Jisu-1129 <115052208+Jisu-1129@users.noreply.github.com>
2022-10-19 09:27:44 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,30 @@
+let counter2 = 0
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    counter = 0
-    while (counter <= 4) {
-        led.plot(counter, 0)
-        basic.pause(1000)
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
         basic.clearScreen()
+        led.plot(0, counter2)
+        basic.pause(100)
+        counter2 += 1
+    }
+    if (counter2 == 5) {
+        counter2 = 0
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(counter, 0)
+        basic.pause(100)
         counter += 1
+    }
+    if (counter == 5) {
+        counter = 0
     }
 })
